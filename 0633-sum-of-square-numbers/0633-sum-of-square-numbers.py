670e54a4117eaea01c9b2a90bd54323192845fd4
Shlok Shivkar <91182775+Shlok-21@users.noreply.github.com>
2024-06-17 13:04:49 +0530

    Time: 712 ms (5%), Space: 36.1 MB (5.64%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,13 +1,11 @@
 import numpy as np
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        '''if c == 0:
-            return True'''
+
         flag = 0
         for a in range(int(np.sqrt(c)+1)):
             b = np.sqrt(c- a*a)
             if b == int(b):
-                flag = 1
-                
+                return True        
         if flag == 0: return False
-        else : return True
+        
