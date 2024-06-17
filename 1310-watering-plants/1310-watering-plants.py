0bdc2089dac105ac4dcde6e6ce7f4f94707115e8
Shlok Shivkar <91182775+Shlok-21@users.noreply.github.com>
2024-06-17 20:48:02 +0530

    Time: 64 ms (5.91%), Space: 16.6 MB (75.05%) - LeetHub

--- 1310-watering-plants/1310-watering-plants.py
+++ 1310-watering-plants/1310-watering-plants.py
@@ -4,12 +4,8 @@ class Solution:
         steps = 0
         for i in range(len(plants)-1):
             steps +=1
-            print(f'now at plant {plants[i]} with steps {steps}')
-            capacity -= plants[i]
-            print(f'capacity is now {capacity} \n')
-            
+            capacity -= plants[i]            
             if capacity < plants[i+1]:
                 steps += (i+1)*2
                 capacity = refill
-
         return(steps+1)
