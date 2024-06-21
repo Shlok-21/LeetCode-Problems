1da067abd08c1a3390b288f1b9ba134cacc5c137
Shlok Shivkar <91182775+Shlok-21@users.noreply.github.com>
2024-06-21 15:18:28 +0530

    Time: 6271 ms (5.04%), Space: 18.8 MB (82.99%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
@@ -1,23 +1,14 @@
+from typing import List
+
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
-        summ, max_ans, max_index = 0, 0, 0
-        for index, i in enumerate(range(len(customers)- minutes+1)):
-            window_customers = customers[i:i+minutes]
-            window_grumpy = grumpy[i:i+minutes]
-            summ = 0
-            for g_index in range(len(window_grumpy)):
-                
-                if window_grumpy[g_index] == 1:
-                    summ += window_customers[g_index]
-                if max_ans < summ:
-                    max_ans = summ
-                    max_index = index
+        satisfied_customers = sum(customers[i] for i in range(len(customers)) if grumpy[i] == 0)
+        print(satisfied_customers)
 
-        for i in range(max_index , max_index+minutes):
-            grumpy[i] = 0
-        max_ans =0
-        for i in range(len(grumpy)):
-            if grumpy[i] == 0:
-                max_ans += customers[i]
+        max_additional_customers = 0
+        for i in range(len(customers)-minutes+1):
+            additional_satisfied = sum(customers[j] for j in range(i, i+minutes) if grumpy[j] == 1)
+            if max_additional_customers < additional_satisfied:
+                max_additional_customers = additional_satisfied
 
-        return max_ans
+        return satisfied_customers + max_additional_customers
