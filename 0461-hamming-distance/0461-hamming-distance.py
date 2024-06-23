e8adc44ed21dd1bd484e98f040f02523526077ae
Shlok Shivkar <91182775+Shlok-21@users.noreply.github.com>
2024-06-23 19:51:55 +0530

    Time: 39 ms (25.3%), Space: 16.5 MB (77.93%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,28 +1,10 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        binx = []
-        biny = []
+        x = bin(x)[2:].zfill(32)  
+        y = bin(y)[2:].zfill(32)  
 
-        while x>1:
-            rem = x%2
-            binx.append(int(rem))
-            x/=2
-        else:
-            binx.append(int(x))
-        
-        while y>1:
-            rem = y%2
-            biny.append(int(rem))
-            y/=2
-        else:
-            biny.append(int(y))      
-
-        while len(binx) > len(biny):
-            biny.append(0)
-        while len(biny) > len(binx):
-            binx.append(0)
-        count = 0
-        for i in range(len(binx)):
-            if binx[i] != biny[i]:
-                count +=1
-        return count
+        summ = 0
+        for i, j in zip(x,y):
+            if i != j:
+                summ +=1
+        return int(summ)
