1c6624b651e8f89c9d2fe723aaf89854d603eed3
Shlok Shivkar <91182775+Shlok-21@users.noreply.github.com>
2024-06-30 18:21:23 +0530

    Time: 43 ms (6.76%), Space: 16.6 MB (17.94%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        s = ''
+        for i in range(len(command)):
+            if command[i].isalpha():
+                s+= command[i]
+            elif command[i] == '(' and command[i+1] == ')':
+                s+= 'o'
+        return s
