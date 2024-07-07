46babd6152f5d3cb31ed643bd1d6d5f51a88b265
Shlok Shivkar <91182775+Shlok-21@users.noreply.github.com>
2024-07-07 14:35:18 +0530

    Time: 401 ms (75.64%), Space: 0B (100%) - LeetHub

---
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+
+select p.product_name, sum(o.unit) as unit from products p
+join orders o
+on p.product_id = o.product_id
+where year(order_date) = 2020 and month(order_date) = 02
+group by p.product_name
+having sum(o.unit) > 99
